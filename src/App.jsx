84168b6cb3cb9bf3f84fd303b68effdbf91dc503
Mariusz Rojewski <rojewski@webkon.eu>
2023-08-07 15:01:39 +0200

    02 BB

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
+import React from 'react';
+import data from "./data"
+import List from "./List";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = React.useState(data)
+
+  console.log("people: ", people)
+
+  return (
+    <main>
+        <section className='container'>
+          <h3>Lista {people.length} największych kurwi na świecie</h3>
+          <List people={people} />
+          <button 
+            type='button' 
+            className='btn btn-block' 
+            onClick={() => setPeople([])}
+          >
+            Clear All
+          </button>
+      </section>
+    </main>
+  );
 };
 export default App;
